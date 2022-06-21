437652ca340c5cc4deb7da15412c4db2ccc8171f
Andrew Strong <andrewstrongdev@gmail.com>
2022-06-21 17:03:13 -0600

    maybe last commit

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -301,6 +301,18 @@ let loser = 'Glimmer'
 */
 
 //CODE HERE
+function theEliminator(contestants, loser) {
+  for (let i = 0; i < contestants.length; i++) {
+    if (contestants[i] === loser) {
+      contestants.splice(i, 1)
+
+  }
+
+
+}
+return contestants
+}
+console.log(theEliminator(contestants, loser))
 
 
 
@@ -312,6 +324,8 @@ let sampleString = "Hi, my name is Kylo."
 */
 
 //CODE HERE
+let allUpperCase = (str) => str.toUpperCase()
+console.log(allUpperCase(sampleString))
 
 
 ////////////////// PROBLEM 18 ////////////////////
@@ -325,21 +339,45 @@ let sampleString = "Hi, my name is Kylo."
   return 'must provide a valid email address'
 */
 
+function emailCheck (email) {
+  email = String(email)
+  email.trim()
+  if (email.includes('@')) {
+    return 'email verified'
+  } else {
+    return 'must provide a valid email address'
+  }
+}
+console.log(emailCheck("asdf@"))
+
+
 ////////////////// PROBLEM 19 ////////////////////
 /*
-  Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were able to purchase.
+  Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with 
+  a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, 
+  which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs 
+  you were able to purchase.
   Create a variable called `totalFrogs` and set it equal to your function invoked, passing in the amount of gold you are willing to spend.
 */
 
 //CODE HERE
-
+function froggy(gold){
+  return  Math.floor(gold/3)
+}
+let totalFrogs = froggy(123)
+console.log(totalFrogs)
 
 ////////////////// PROBLEM 20 ////////////////////
 /*
-  You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
+  You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, 
+  the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. 
+  You would just be able to purchase 1 product. Re-write the function you used in the previous problem 
+  (give it the same name, just add a 2 to the end of it) that fixes this bug. 
+  Invoke the function and store the returned value to a variable called `totalFrogs2`.
 */
 
 //CODE HERE
+
 
 
 ////////////////// PROBLEM 21 ////////////////////
